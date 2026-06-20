affa602e77ae86005e2dbeaf1bcf354cbee57ab0
anna <annajose2024@cs.sjcetpalai.ac.in>
2026-06-20 12:10:28 +0530

    Add project files.

---
+++ Weatherforecastdemo/Program.cs
@@ -0,0 +1,51 @@
+
+namespace Weatherforecastdemo
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+            // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+            builder.Services.AddOpenApi();
+                
+            builder.Services.AddCors(options =>
+            {
+                options.AddPolicy("ReactPolicy", policy =>
+                {
+
+
+                    policy.WithOrigins(
+                        
+                        "https://weatherforecastclient-eaceg8aaascjg7cy.centralindia-01.azurewebsites.net")
+
+                          .AllowAnyHeader()
+                          .AllowAnyMethod();
+                });
+            });
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.MapOpenApi();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseCors("ReactPolicy");
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
